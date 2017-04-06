d07761dde31799281ac207b9841333e18715edaa
bajurar <bajurar@ee.pw.edu.pl>
2017-04-06 23:15:27 +0200

    Przygotowanie metody podmiany słów

--- scripts/liveChatScripts.js
+++ scripts/liveChatScripts.js
@@ -1,4 +1,6 @@
 var domain = 'bajurar.github.io';
+var template = {};
+var emotes = {};
 
 function loadIFrame() {
 	let searchParams = new URLSearchParams(window.location.search);
@@ -9,6 +11,33 @@ function loadIFrame() {
 	}
 }
 
+function loadTwitchApi() {
+	$.get('https://twitchemotes.com/api_cache/v2/global.json', function(data){
+		template = data.template;
+		emotes = data.emotes;
+	});
+}
+
 $(document).ready(function() {
+	loadTwitchApi();
 	loadIFrame();
 });
+
+function addEmotes(spanObj) {
+	var wordsArray = $(spanObj.html().split(" "));
+	wordsArray.each(function(i, word){
+		if(word in emotes) {
+			var imgHtml = '<img class="twitch-emote" src="' + template['small'].replace('{image_id}', iemotes[word].image_id) + '" />';
+			if(i==0){
+				spanObj.html(spanObj.html().replace(word+" ", imgHtml + " "));
+			}
+			else if(i==wordsArray.length-1){				
+				spanObj.html(spanObj.html().replace(" "+word, " " +imgHtml));
+			}
+			else {
+				spanObj.html(spanObj.html().replace(" "+word+" ", " " +imgHtml+ " "));				
+			}
+		}
+	});
+});
+
